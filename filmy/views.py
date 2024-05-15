bbab622a382828c4c6860613d1ab5d2a77d5a2fd
Thiago Gonzaga dos Santos <thiagogsantos2013@gmail.com>
2024-05-15 17:28:09 -0300

    correção de estruturação e alguns aspectos visuais da tela inicial

--- filmy/views.py
+++ filmy/views.py
@@ -8,10 +8,10 @@ def HomeScreen(request):
     api_key = "e06d43b09426bf8ef8c1b0748ebf262d"
     url = f"https://api.themoviedb.org/3/movie/upcoming?api_key={api_key}&language=pt-BR&page=1"
     
-    headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer e06d43b09426bf8ef8c1b0748ebf262d"
-}
+    ##headers = {
+    ##"accept": "application/json",
+    ##"Authorization": "Bearer e06d43b09426bf8ef8c1b0748ebf262d"
+##}
 
     response = requests.get(url)
     upcomingMovies = response.json()
