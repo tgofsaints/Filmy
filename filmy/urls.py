0076165a324a8be3a3308b6e8b051cbda97153cd
Thiago Gonzaga dos Santos <thiagogsantos2013@gmail.com>
2024-05-15 00:02:09 -0300

    FEAT: implementação da tela inicial

--- filmy/urls.py
+++ filmy/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from filmy.views import HomeScreen
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HomeScreen, name="HomeScreen"),
+    path('admin/', admin.site.urls)
 ]
--- filmy/views.py
+++ filmy/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponse
 
-def Login(request):
-    return render(request, 'login.html')
+from django.shortcuts import render
+import requests
+
+def HomeScreen(request):
+    api_key = "e06d43b09426bf8ef8c1b0748ebf262d"
+    url = f"https://api.themoviedb.org/3/movie/upcoming?api_key={api_key}&language=pt-BR&page=1"
+    
+    headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer e06d43b09426bf8ef8c1b0748ebf262d"
+}
+
+    response = requests.get(url)
+    upcomingMovies = response.json()
+    
+    context = {
+        'dates': upcomingMovies['dates'],
+        'movies': upcomingMovies['results']
+    }
+
+    return render(request, 'telainicial.html', context)
+    
+
+
+
